51140ef504c1d50c46405acc82f055db541f564e
MCT <celaltok2@gmail.com>
2020-05-05 22:36:59 +0300

    created

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by MCT on 5.05.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.backgroundColor = .red
+    }
+
+
+}
+
