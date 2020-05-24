4170038ead7608335d94df644dbed179dec82f6d
MCT <celaltok2@gmail.com>
2020-05-24 14:45:52 +0300

    created ProfileController

---
+++ TwitterClone/Controller/ProfileController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileController.swift
+//  TwitterClone
+//
+//  Created by MCT on 23.05.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import UIKit
+
+class ProfileController: UICollectionViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .red
+    }
+}
