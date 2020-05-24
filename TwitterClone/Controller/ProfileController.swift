d68c91fe0422ed0e6373cf4fdbbd00522be10354
MCT <celaltok2@gmail.com>
2020-05-24 16:34:18 +0300

    edited profileController

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
+private let reuseIdentifierCell = "TweetCell"
+
 class ProfileController: UICollectionViewController {
     
+    //MARK: - Properties
     
+    //MARK: - Lidecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.backgroundColor = .red
+        configureCollectionView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    //MARK: - Helpers
+    
+    func configureCollectionView(){
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifierCell)
+    }
+}
+
+    //MARK: - UICollectionVieDataSource/Delegate
+extension ProfileController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifierCell, for: indexPath) as! TweetCell
+         
+        return cell
+    }
+    
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height * 0.3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
+    }
+    
+    
 }
---
+++ TwitterClone/View/ProfileHeader.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileHeader.swift
+//  TwitterClone
+//
+//  Created by MCT on 24.05.2020.
+//  Copyright © 2020 MCT. All rights reserved.
+//
+
+import Foundation
